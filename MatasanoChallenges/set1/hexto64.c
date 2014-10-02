fb12b3f63cc0a75eb44a0db11fd80033c8bd711b
Andrew Akers <andrew@akrs.me>
2014-10-01 20:19:48 -0700

    still not working. might be closer now.

--- MatasanoChallenges/set1/hexto64.c
+++ MatasanoChallenges/set1/hexto64.c
@@ -5,9 +5,9 @@
 
 uint8_t parseHexDigit(char in) {
     if (in > 'a' && in < 'g') {
-        return 'a' - in;
+        return in - 'a' + 0xA;
     } else if (in > 'A' && in < 'G') {
-        return 'A' - in;
+        return in - 'A' + 0xA;
     } else if (in > '0' && in <= '9') {
         return in - '0';
     }
@@ -16,34 +16,43 @@ uint8_t parseHexDigit(char in) {
 }
 
 uint8_t* parseHexString(char* txt, int txtlen) {
+    printf("I'm parsing hex.\n");
     uint8_t* result = malloc(txtlen / 2);
-    printf("%s\n", "Started parsing. I have not died yet.");
     for (int posInInput, posInResult = 0; posInInput < txtlen; posInInput += 2) {
         result[posInResult] += parseHexDigit(txt[posInInput]) * 0x10;
         result[posInResult++] += parseHexDigit(txt[posInInput + 1]);
-        printf("Parsed %c%c as %x\n", txt[posInInput], txt[posInInput+1], result[posInResult - 1]);
     }
-
+    printf("I've finished parsing hex.\n");
     return result;
 }
 
 char* base64toText(uint8_t *input, int input_length) {
     char convert[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
     char *result = malloc((size_t)(input_length / 1.5 + 1));
-    int posInInput, posInResult = 0;
-    for (posInInput = 0; posInInput + 2 <= input_length; posInInput += 3) {
-        uint8_t toConvert = input[posInInput] / 0x4;
-        printf("Converting: %d\nRaw hex for position %d: %x\n", toConvert, posInInput, input[posInInput]);
+    printf("I just allocated memory. Gonna convert this.\n");
+    int posInResult = 0;
+    int posInInput = 0;
+    printf("Setup some loop variables.\n");
+    while (posInInput + 2 < input_length) {
+        printf("I'm in ur loop, converting ur stuff.\n");
+        uint8_t toConvert = input[posInInput] >> 2;
+        printf("The number I'm converting is: %d\n", toConvert);
         result[posInResult++] = convert[toConvert];
-        toConvert = (input[posInInput] % 0x4) * 0x10 + input[++posInInput] / 0x10;
-        printf("Converting: %d\nRaw hex for position %d: %x\n", toConvert, posInInput, input[posInInput]);
+        printf("I got: %c, from %d\n", convert[toConvert], toConvert);
+        toConvert = ((input[posInInput] & 0x3) << 4) + (input[posInInput + 1] >> 4); // Grab the rightmost two bits of this position and the leftmost two bits of the next.
+        posInInput++;
+        printf("The number I'm converting is: %d\n", toConvert);
         result[posInResult++] = convert[toConvert];
-        toConvert = input[posInInput] % 0x10 + input[++posInInput] / 0x40;
-        printf("Converting: %d\nRaw hex for position %d: %x\n", toConvert, posInInput, input[posInInput]);
+        printf("I got: %c, from %d\n", convert[toConvert], toConvert);
+        toConvert = (input[posInInput] & 0xff << 2) + (input[posInInput + 1] >> 6);
+        posInInput++;
+        printf("The number I'm converting is: %d\n", toConvert);
         result[posInResult++] = convert[toConvert];
-        toConvert = input[posInInput] % 0x40;
-        printf("Converting: %d\nRaw hex for position %d: %x\n", toConvert, posInInput, input[posInInput]);
-        result[posInResult] = convert[toConvert];
+        printf("I got: %c, from %d\n", convert[toConvert], toConvert);
+        toConvert = input[posInInput] & 0x3f;
+        result[posInResult++] = convert[toConvert];
+        printf("The number I'm converting is: %d\n", toConvert);
+        printf("I got: %c, from %d\n", convert[toConvert], toConvert);
     }
 
     result[posInResult] = 0x0;
@@ -53,7 +62,9 @@ char* base64toText(uint8_t *input, int input_length) {
 }
 
 char* stringyHexTo64(char* txtin, int txtinlen) {
+    printf("I'm trying to convert.\n");
     uint8_t *parsed = parseHexString(txtin, txtinlen);
+    printf("I have some parsed data.\n");
     char *result = base64toText(parsed, txtinlen / 2);
     free(parsed);
     return result;
@@ -63,11 +74,14 @@ int main(int argc, char const *argv[]) {
     char *input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     printf("%s\n", "Just setup input. I have not died yet.");
     int input_length = 96;
-    char* result = stringyHexTo64(input, input_length);
+    char *result = stringyHexTo64(input, input_length);
 
-    printf("%s\n",result);
+    printf("Finished converting.\n");
+
+    printf("%s\n", result);
 
     free(result);
+
 
     return 0;
 }
