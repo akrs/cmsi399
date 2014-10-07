1c8af05b1f45460b785e9273b4126bd7d1225f58
Andrew Akers <andrew@akrs.me>
2014-10-06 23:04:15 -0700

    refactor

--- MatasanoChallenges/set1/breakSingleXOR.c
+++ MatasanoChallenges/set1/breakSingleXOR.c
@@ -4,12 +4,7 @@
 #include <smmintrin.h>
 #include "utils.h"
 
-
-
-int main(int argc, char const *argv[]) {
-    uint8_t* input = parseHexString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", 68);
-    int inputlen = 34;
-
+char* breakSingleXOR(uint8_t* input, int inputlen) {
     uint8_t bestKey = 0;
     double bestKeyScore = 0;
     uint8_t* possibleMessage = malloc(inputlen);
@@ -24,8 +19,17 @@ int main(int argc, char const *argv[]) {
 
     xorSingleValueInPlace(input, inputlen, bestKey, possibleMessage);
 
-    printf("Key: %d, Score: %f, Message: %s\n", bestKey, bestKeyScore, possibleMessage);
-    free(possibleMessage);
+    return (char*)possibleMessage;
+}
+
+int main(int argc, char const *argv[]) {
+    uint8_t* input = parseHexString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", 68);
+    int inputlen = 34;
+
+    char* message = breakSingleXOR(input, inputlen);
+
+    printf("Message: %s\n", message);
+    free(message);
 
     return 0;
 }
---
+++ MatasanoChallenges/set1/breakSingleXOR.h
@@ -0,0 +1,2 @@
+/* Returns a string containing the (probable) message */
+char* breakSingleXOR(uint8_t* input, int inputlen);
